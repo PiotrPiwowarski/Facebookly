34459df01327dc7b837c140e93edaca8b378e69f
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2023-12-14 17:11:12 +0100

    new commit

--- src/main/java/pl/piwowarski/facebookly/model/enums/Reaction.java
+++
@@ -1,7 +0,0 @@
-package pl.piwowarski.facebookly.model.enums;
-
-public enum Reaction {
-
-    LIKE,
-    DISLIKE
-}
--- src/main/java/pl/piwowarski/facebookly/model/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package pl.piwowarski.facebookly.model.enums;
-
-public enum Role {
-
-    USER,
-    ADMIN
-}
