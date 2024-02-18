a317aacc026f3ddf0308501c91d010cd85de7933
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-02-18 16:08:59 +0100

    new commit

--- src/main/java/pl/piwowarski/facebookly/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/pl/piwowarski/facebookly/exceptionHandler/ApiExceptionHandler.java
@@ -1,6 +1,5 @@
 package pl.piwowarski.facebookly.exceptionHandler;
 
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
